e78398d306bdacf5f297d7cf5c779b241d945205
longwatermelon <longwatermelon1@gmail.com>
2022-03-19 09:32:04 -0700

    incredible

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def random_phrase() -> list:
     nouns = open("nouns.txt", "r").readlines()
     actions = open("actions.txt", "r").readlines()
 
-    ret = f"When the {random.choice(nouns)[:-1]} {random.choice(actions)[:-1]} you"
+    ret = f"When the {random.choice(nouns)[:-1]} {random.choice(actions)[:-1]}s you"
 
     return [[(0, 0), ret]]
 
@@ -20,17 +20,19 @@ def generate_meme(meme) -> list:
     noun2 = random.choice(open('nouns.txt', 'r').readlines())[:-1]
     action = random.choice(open('actions.txt', 'r').readlines())[:-1]
     noun3 = random.choice(open('nouns.txt', 'r').readlines())[:-1]
-    
+    adjective = random.choice(open('adjs.txt', 'r').readlines())[:-1]
+
     texts = {
         "peace": random_phrase(),
         "uno": [[(100, 150), f"{verb} {noun}s"], [(300, 20), f"{noun2}s"]],
-        "bonjour": [[(0, 0), f"{noun3}s when the {noun} {action} the {noun2}"]]
+        "bonjour": [[(0, 0), f"{noun3}s when the {noun} {action}s the {noun2}"]],
+        "incredible": [[(50, 150), f"Me when I {action} the {noun}"], [(600, 150), f"It's {adjective}"]]
     }
 
     return texts[meme]
 
 def main():
-    meme = random.choice(["peace", "uno", "bonjour"])
+    meme = random.choice(["peace", "uno", "bonjour", "incredible"])
     caption = generate_meme(meme)
 
     img = Image.open(f"formats/{meme}.png")
