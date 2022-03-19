bd65bf0f5a775260a2881b53a5d3d562e091cdb1
longwatermelon <longwatermelon1@gmail.com>
2022-03-19 11:03:19 -0700

    lmaoo

--- video.py
+++ video.py
@@ -3,6 +3,8 @@ import sys
 from PIL import Image
 import os
 import moviepy.editor as mpe
+import pyttsx3
+import time
 
 avg_w = 0
 avg_h = 0
@@ -27,18 +29,52 @@ for file in os.listdir("memes"):
     imr.save(f"video/{count}.png")
     count += 1
 
-for v in os.listdir("video"):
-    out = cv2.VideoWriter("out.avi", cv2.VideoWriter_fourcc(*'DIVX'), 0.2, (avg_w, avg_h))
-    
-    for file in os.listdir("video"):
-        out.write(cv2.imread(f"video/{file}"))
+count = 0
+engine = pyttsx3.init()
+engine.setProperty('rate', 125)
+engine.setProperty('volume', 1.0)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+# engine.say('hello world')
+# engine.runAndWait()
+# engine.stop()
 
+clips = list()
+for i in range(len(os.listdir("video"))):
+    out = cv2.VideoWriter(f"videos/{i}.avi", cv2.VideoWriter_fourcc(*"DIVX"), 0.2, (avg_w, avg_h))
+    out.write(cv2.imread(f"video/{i}.png"))
     out.release()
 
+    clip = mpe.VideoFileClip(f"videos/{i}.avi")
+    # engine.save_to_file("hello world", "tts.mp3")
+    # engine.runAndWait()
+    # time.sleep(1)
+    # audio = mpe.AudioFileClip("tts.mp3")
 
-clip = mpe.VideoFileClip("out.avi")
+    # fclip = clip.set_audio(audio)
+    clips.append(clip)
+
+    print(f"Clip {i}")
+
+    # out = cv2.VideoWriter("out.avi", cv2.VideoWriter_fourcc(*'DIVX'), 0.2, (avg_w, avg_h))
+    
+    # for file in os.listdir("video"):
+    #     out.write(cv2.imread(f"video/{file}"))
+
+    # out.release()
+
+final = mpe.concatenate_videoclips(clips, method="compose")
 audio = mpe.AudioFileClip("music.mp3")
-audio = audio.set_duration(clip.duration)
+audio = audio.set_duration(final.duration)
 final_audio = mpe.CompositeAudioClip([audio])
-final_clip = clip.set_audio(final_audio)
-final_clip.write_videofile("out.mp4")
+final = final.set_audio(final_audio)
+
+final.write_videofile("out.mp4")
+
+# clip = mpe.VideoFileClip("out.avi")
+# audio = mpe.AudioFileClip("music.mp3")
+# audio = audio.set_duration(clip.duration)
+# final_audio = mpe.CompositeAudioClip([audio])
+# final_clip = clip.set_audio(final_audio)
+# final_clip.write_videofile("out.mp4")
+
