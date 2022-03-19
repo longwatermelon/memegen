9c2de481bf69fc60175e4db37bb40b3e1e8e2f7d
longwatermelon <longwatermelon1@gmail.com>
2022-03-18 22:07:42 -0700

    spongebob format

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
 from PIL import Image, ImageDraw, ImageFont
 import random
 
-def random_phrase():
+def random_phrase() -> list:
     nouns = open("nouns.txt", "r").readlines()
     actions = open("actions.txt", "r").readlines()
 
     ret = f"When the {random.choice(nouns)[:-1]} {random.choice(actions)[:-1]} you"
 
-    return ret
+    return [[(0, 0), ret]]
 
-def generate_meme(meme):
+def random_nouns(count, x, spacing) -> list:
+    nouns = open("nouns.txt", "r").readlines()
+
+    return [[(x, spacing * i), random.choice(nouns)] for i in range(count)]
+
+def generate_meme(meme) -> list:
     texts = {
-        "peace": random_phrase()
+        "peace": random_phrase(),
+        "spongebob": random_nouns(5, 350, 230)
     }
 
     return texts[meme]
 
 def main():
-    caption = generate_meme("peace")
+    meme = random.choice(["peace", "spongebob"])
+    caption = generate_meme(meme)
 
-    img = Image.open("formats/peace.png")
+    img = Image.open(f"formats/{meme}.png")
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype('impact.ttf', size=18)
-    draw.text((0, 0), caption, 'white', font)
+    print(caption)
+
+    for l in caption:
+        print(l[0])
+        draw.text((l[0][0] - 1, l[0][1] - 1), l[1], 'black', font)
+        draw.text((l[0][0] + 1, l[0][1] - 1), l[1], 'black', font)
+        draw.text((l[0][0] - 1, l[0][1] + 1), l[1], 'black', font)
+        draw.text((l[0][0] + 1, l[0][1] + 1), l[1], 'black', font)
+
+        draw.text(l[0], l[1], 'white', font)
     img.save('out.png')
 
 main()
